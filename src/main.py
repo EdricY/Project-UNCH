44a782e16b335a5db4715b3c7624d97066106b79
RedInquisitive <aj.walter@aol.com>
2015-10-26 20:34:30 -0500

    Made a circle? Circles?

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import sys
+import time
 def printxy(x, y, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
      sys.stdout.flush()
@@ -6,18 +7,7 @@ def refresh():
 	raw_input()
 	print(chr(27) + "[2J")
 	
-for x in range (0,25):
-	for y in range(0,50):
-		sys.stdout.write('.')
-	print
-printxy(10,10,"Hello!")
-refresh()
-printxy(10,10,"......")
-printxy(11,10,"Hello!")
-refresh()
-printxy(11,10,"......")
-printxy(12,10,"Hello!")
-refresh()
-printxy(12,10,"......")
-printxy(13,10,"Hello!")
-refresh()
+for d in range(0,360)
+	printxy(math.floor(10 + (11 * math.cos(d))),math.floor(10 - (11 * math.sin(d))),"X")
+	refresh()
+	time.sleep(0.1)
