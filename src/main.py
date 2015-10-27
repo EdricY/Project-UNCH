027ed44a7561dbd4a65ca9d5f01186c01655d12f
RedInquisitive <aj.walter@aol.com>
2015-10-26 20:22:27 -0500

    Changed output to clear. Clears things maybe?

--- src/main.py
+++ src/main.py
@@ -2,18 +2,22 @@ import sys
 def printxy(x, y, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
      sys.stdout.flush()
+def refresh():
+	raw_input()
+	print(chr(27) + "[2J")
+	
 for x in range (0,25):
 	for y in range(0,50):
 		sys.stdout.write('.')
 	print
 printxy(10,10,"Hello!")
-raw_input()
+refresh()
 printxy(10,10,"......")
-printxy(10,11,"Hello!")
-raw_input()
-printxy(10,11,"......")
-printxy(10,12,"Hello!")
-raw_input()
-printxy(10,12,"......")
-printxy(10,13,"Hello!")
-raw_input()
+printxy(11,10,"Hello!")
+refresh()
+printxy(11,10,"......")
+printxy(12,10,"Hello!")
+refresh()
+printxy(12,10,"......")
+printxy(13,10,"Hello!")
+refresh()
