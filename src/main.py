17b635304cfa923c62828a295a1670605d9dc79e
RedInquisitive <aj.walter@aol.com>
2015-10-26 20:07:24 -0500

    Made print location. printxy.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
+import sys
 for x in range (0,25):
 	for y in range(0,25):
 		print '.',
 	print
+print "Setup complete, please wait..."
+def printxy(x, y, text):
+     sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+     sys.stdout.flush()
+printxy(10,10,"Hello!")
