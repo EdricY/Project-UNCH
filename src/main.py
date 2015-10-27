69cf8ece271b032cba5552ecfd683e09d35a5d90
EdricY <yu.edric@gmail.com>
2015-10-26 21:05:19 -0500

    circle thing works

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
+import math
 import sys
 import time
-def printxy(x, y, text):
+def printxy(y, x, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
      sys.stdout.flush()
 def refresh():
-	raw_input()
-	print(chr(27) + "[2J")
+        raw_input()
+        print(chr(27) + "[2J")
 def clear():
-	print(chr(27) + "[2J")
-	
+        print(chr(27) + "[2J")
+
 for d in range(0,360):
-	d = math.radians(d)
-	printxy(math.floor(10 + (11 * math.cos(d))),math.floor(10 - (11 * math.sin(d))),"X")
-	clear()
-	time.sleep(0.1)
+        d = math.radians(d)
+        printxy(21,11,math.cos(d))
+        printxy(21,12,math.sin(d))
+        printxy(math.floor(21 + 2*(10 * math.cos(d))),math.floor(11 -(10 * math.sin(d))),"X")
+        time.sleep(.1)
+        clear()
