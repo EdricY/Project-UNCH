e3d0fd8f14a660ca300143efb0ec19a0ec5e44e0
RedInquisitive <aj.walter@aol.com>
2015-10-26 21:32:00 -0500

    Fixed clear. And it wipes.

--- src/main.py
+++ src/main.py
@@ -4,14 +4,12 @@ import time
 def printxy(y, x, text):
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
 	sys.stdout.flush()
-def wipe():
+def clear():
 	for y in range(1,26):
 		printxy(1,y,"                                                            ")
 def refresh():
 	raw_input()
-	wipe()
-def clear():
-	wipe()
+	clear()
 
 for d in range(0,360):
 	d = math.radians(d)
