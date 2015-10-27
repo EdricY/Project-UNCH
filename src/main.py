bd26152951b3088a553b0b18cf7b4d1e6ff75d5d
RedInquisitive <aj.walter@aol.com>
2015-10-26 20:17:40 -0500

    Print test animation? test.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import sys
-for x in range (0,25):
-	for y in range(0,25):
-		print '.',
-	print
-print "Setup complete, please wait..."
 def printxy(x, y, text):
      sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
      sys.stdout.flush()
+for x in range (0,25):
+	for y in range(0,50):
+		sys.stdout.write('.')
+	print
 printxy(10,10,"Hello!")
+raw_input()
+printxy(10,10,"......")
+printxy(10,11,"Hello!")
+raw_input()
+printxy(10,11,"......")
+printxy(10,12,"Hello!")
+raw_input()
+printxy(10,12,"......")
+printxy(10,13,"Hello!")
+raw_input()
