749f3d7507cd3f66e8197f42b3294ce20a9d2e64
RedInquisitive <aj.walter@aol.com>
2015-11-09 16:01:46 -0600

    Added updater.

---
+++ Update-UNCH.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf ./Project-UNCH
+git clone https://github.com/EdricY/Project-UNCH.git
+cd ./Project-UNCH/src
+echo "Starting Project-UNCH!"
+python main.py
+echo "Project-UNCH has quit."
+cd ../
