5794a5f9c8c2756dccec3f14c7047f7f80edd6d0
EdricY <yu.edric@gmail.com>
2015-11-09 15:48:54 -0600

    No more circle stuff blah

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import math
 import sys
 import time
+import subprocess
 def printxy(y, x, text):
         sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
         sys.stdout.flush()
@@ -10,21 +11,24 @@ def wipe():
 def refresh():
         raw_input()
         wipe()
+def writesave():
+
+def readsave():
+
+def update():
+	
+def draw():
+	f = open('workfile', 'w')
+	print f
+	
 #game loop
 FRAMES_PER_SECOND = 1
 game_running = True
-c=0
 while game_running:
-        startTime=time.time()
-        for a in range(0,30):
-                wipe()
-                t = math.radians(a+30*c)
-                printxy(21,11,math.cos(t))
-                printxy(21,12,math.sin(t))
-                printxy(math.floor(21.5 + 2*(10 * math.cos(t))),math.floor(11.5 -(10 * math.sin(t))),"X")
-                time.sleep(.01)
-        c = c+1
-        endTime=time.time()
-        timeElapsed=endTime-startTime
-        sleepTime=1/FRAMES_PER_SECOND-timeElapsed
-        time.sleep(sleepTime)
+	startTime=time.time()
+	endtime=time.time()
+	timeElapsed=endTime-startTime
+	sleepTime=1/FRAMES_PER_SECOND-timeElapsed
+	time.sleep(sleepTime)
+	update()
+	draw()
