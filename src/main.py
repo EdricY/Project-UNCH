dd9e133bc5981416fa1a3bc4d20364de48619e0f
EdricY <yu.edric@gmail.com>
2015-11-09 17:58:51 -0600

    Load mobs duh

--- src/main.py
+++ src/main.py
@@ -2,33 +2,50 @@ import math
 import sys
 import time
 import subprocess
-def printxy(y, x, text):
-        sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
-        sys.stdout.flush()
-def wipe():
-        for y in range(1,26):
-                printxy(1,y,"                                                            ")
-def refresh():
-        raw_input()
-        wipe()
-def writesave():
+import glob
 
-def readsave():
-
-def update():
-	
-def draw():
-	f = open('workfile', 'w')
-	print f
-	
-#game loop
 FRAMES_PER_SECOND = 1
 game_running = True
+
+mobfiles = glob.glob("./resources/mobs/*.txt")
+ifile=0
+mobs=[]
+for i in mobfiles:
+	mobs.append([])
+	f=open(i)
+	for line in f:
+		mobs[ifile].append(line)
+	ifile+=1
+	f.close()
+currentMob=0
+def printxy(y, x, text):
+	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+	sys.stdout.flush()
+def wipe():
+	for y in range(1,26):
+		printxy(1,y,"                                                            ")
+def refresh():
+	raw_input()
+	wipe()
+def writesave():
+	todo="write"
+def readsave():
+	todo="read"
+def update():
+	todo="everything"
+def draw():
+	for i in range(0,10):
+		printxy(0,i+1,mobs[currentMob][i])
+#game loop
+
 while game_running:
 	startTime=time.time()
-	endtime=time.time()
+
+	update()
+	draw()
+	currentMob=currentMob+1%ifile
+
+	endTime=time.time()
 	timeElapsed=endTime-startTime
 	sleepTime=1/FRAMES_PER_SECOND-timeElapsed
 	time.sleep(sleepTime)
-	update()
-	draw()
