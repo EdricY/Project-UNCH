64a4fca52e2a321389ab04b3e7a181c5bf36f551
RedInquisitive <aj.walter@aol.com>
2015-11-10 16:51:04 -0600

    Added some comments, removed stuff.

--- src/main.py
+++ src/main.py
@@ -1,54 +1,64 @@
+#System imports
 import math
 import sys
 import time
 import os
 import glob
 
-os.system('clear')
-FRAMES_PER_SECOND = 1
-game_running = True
+#Module imports
+import gui
 
-mobfiles = glob.glob("../resources/mobs/*.txt")
-ifile=0
-mobs=[]
-for i in mobfiles:
-	mobs.append([])
+#Platform code
+os.system('clear')
+
+#Global variables
+FRAMES_PER_SECOND = 1
+GAME_RUNNING = True
+MOB_FILES = glob.glob("../resources/mobs/*.txt")
+COUNT_FILES=0
+MOBS=[]
+
+#Mob loading
+for i in MOB_FILES:
+	MOBS.append([])
 	f=open(i)
 	for line in f:
-		mobs[ifile].append(line)
-	ifile+=1
-	
+		MOBS[COUNT_FILES].append(line)
+	COUNT_FILES+=1
 	f.close()
-currentMob=0
 
+#Definitions and methods
 def printxy(y, x, text):
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
 	sys.stdout.flush()
+
 def wipe():
 	for y in range(1,26):
 		printxy(1,y,"                                                            ")
+
 def refresh():
 	raw_input()
 	wipe()
+
 def writesave():
 	todo="write"
+
 def readsave():
 	todo="read"
+
 def update():
 	todo="everything"
+
 def draw():
 	wipe()
-	for i in range(0,10):
-		printxy(0,i+1,mobs[currentMob][i])
-#game loop
+#	for i in range(0,10):
+#		printxy(0,i+1,MOBS[currentMob][i])
 
-while game_running:
+#Main loop
+while GAME_RUNNING:
 	startTime=time.time()
-
 	update()
-
 	draw()
-	currentMob=(currentMob+1)%ifile
 	endTime=time.time()
 	timeElapsed=endTime-startTime
 	sleepTime=1/FRAMES_PER_SECOND-timeElapsed
