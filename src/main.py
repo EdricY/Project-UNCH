6eadd39e911706182bced2b04c5a90a729ced19b
EdricY <yu.edric@gmail.com>
2015-11-09 18:27:52 -0600

    stuff works

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import math
 import sys
 import time
-import subprocess
+import os
 import glob
 
+os.system('clear')
 FRAMES_PER_SECOND = 1
 game_running = True
 
-mobfiles = glob.glob("./resources/mobs/*.txt")
+mobfiles = glob.glob("../resources/mobs/*.txt")
 ifile=0
 mobs=[]
 for i in mobfiles:
@@ -16,8 +17,10 @@ for i in mobfiles:
 	for line in f:
 		mobs[ifile].append(line)
 	ifile+=1
+	
 	f.close()
 currentMob=0
+
 def printxy(y, x, text):
 	sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
 	sys.stdout.flush()
@@ -34,6 +37,7 @@ def readsave():
 def update():
 	todo="everything"
 def draw():
+	wipe()
 	for i in range(0,10):
 		printxy(0,i+1,mobs[currentMob][i])
 #game loop
@@ -42,9 +46,9 @@ while game_running:
 	startTime=time.time()
 
 	update()
-	draw()
-	currentMob=currentMob+1%ifile
 
+	draw()
+	currentMob=(currentMob+1)%12
 	endTime=time.time()
 	timeElapsed=endTime-startTime
 	sleepTime=1/FRAMES_PER_SECOND-timeElapsed
