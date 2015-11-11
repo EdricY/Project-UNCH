16dbc21eddd5dee06ab4508ef26cb2564f8002ae
EdricY <yu.edric@gmail.com>
2015-11-11 16:18:47 -0600

    Update getch.py

--- src/getch.py
+++ src/getch.py
@@ -2,9 +2,6 @@ class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
     def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
             self.impl = _GetchUnix()
 
     def __call__(self): return self.impl()
