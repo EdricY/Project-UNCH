c3ccabb3dd46bd670e97e5de27ca5442834288c2
EdricY <yu.edric@gmail.com>
2015-12-09 17:43:01 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -11,26 +11,7 @@ import random
 #Module imports
 import gui
 import method
-MOB_FILES = glob.glob("../resources/mobs/*.txt")
-COUNT_FILES=0
-CURRENT_MOB=3
-MOBS=[]
-for i in MOB_FILES:
-	MOBS.append([])
-	f=open(i)
-	MOBS[COUNT_FILES].append("&RK"+i[16+i.find("/resources/mobs/"):len(i)-4].capitalize()+"&XX")
-	for line in f:
-		MOBS[COUNT_FILES].append(line)
-	COUNT_FILES+=1
-	f.close()
-MOB_MAX_HP=10
-MOB_HP=MOB_MAX_HP
-Y=int((float(MOB_HP)/float(MOB_MAX_HP))*22.0)
-X=55
-for i in range(22-Y, 22):
-        method.bufferxy(X,i+1,("&GG" if Y > 0.5*22.0 else ("&YY" if Y > 0.25*22.0 else "&RR")) + "XX" + "&XX") #hashtag healthbar
-method.printBuffer()
-time.sleep(1)
-for i in range(1,len(MOBS[CURRENT_MOB])): #mob drawing
-        method.bufferxy(32,4+i,MOBS[CURRENT_MOB][i])
+
+
+method.bufferxy(0,0,"!")
 method.printBuffer()
