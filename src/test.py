610da1582411903a4ff55bf15544d9a6032169a1
EdricY <yu.edric@gmail.com>
2015-12-09 03:58:10 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,4 +1,9 @@
 import method
 
-method.bufferxy(0,0,"!@")
+MOB_MAX_HP=10
+MOB_HP=MOB_MAX_HP
+Y=int((float(MOB_HP)/float(MOB_MAX_HP))*22.0)
+X=55
+for i in range(22-Y, 22):
+  method.bufferxy(X,i+1,  "XX")#("&GG" if Y > 0.5*22.0 else ("&YY" if Y > 0.25*22.0 else "&RR")) + "XX" + "&XX") #hashtag healthbar
 method.printBuffer()
