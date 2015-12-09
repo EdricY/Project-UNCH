3aab789d1b4c8b7d41d68f4815a2c9016292d052
EdricY <yu.edric@gmail.com>
2015-12-09 05:10:13 -0600

    moved health bar draw

--- src/test.py
+++ src/test.py
@@ -23,8 +23,6 @@ for i in MOB_FILES:
 		MOBS[COUNT_FILES].append(line)
 	COUNT_FILES+=1
 	f.close()
-for i in range(1,len(MOBS[CURRENT_MOB])): #mob drawing
-        method.bufferxy(32,4+i,MOBS[CURRENT_MOB][i])
 MOB_MAX_HP=10
 MOB_HP=MOB_MAX_HP
 Y=int((float(MOB_HP)/float(MOB_MAX_HP))*22.0)
@@ -32,3 +30,6 @@ X=55
 for i in range(22-Y, 22):
         method.bufferxy(X,i+1,("&GG" if Y > 0.5*22.0 else ("&YY" if Y > 0.25*22.0 else "&RR")) + "XX" + "&XX") #hashtag healthbar
 method.printBuffer()
+for i in range(1,len(MOBS[CURRENT_MOB])): #mob drawing
+        method.bufferxy(32,4+i,MOBS[CURRENT_MOB][i])
+
