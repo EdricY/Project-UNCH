0f3bf54b0f2ed8125d9f931c32fbef5a214e398b
EdricY <yu.edric@gmail.com>
2015-12-09 22:56:03 -0600

    more arguments

--- src/gui.py
+++ src/gui.py
@@ -5,15 +5,17 @@ import sys
 import method
 
 #Definitions and methods
-def drawgui():
-	for i in range(0,len(GUI_LINES)):
-		method.printxy(0,i+1,GUI_LINES[i])
-def drawtitle():
+
+#def drawgui():
+#	for i in range(0,len(GUI_LINES)):
+#		method.printxy(0,i+1,GUI_LINES[i])
+
+def drawtitle(rows, columns):
 	for i in range(0,len(TITLE_LINES)):
-		method.printxy(0,i+1,TITLE_LINES[i])
-def drawquitmenu():
+		method.printxy(columns/2 - 12, rows/2-29 + i+1,TITLE_LINES[i])
+def drawquitmenu(rows, columns):
 	for i in range(0,len(QUIT_LINES)):
-		method.bufferxy(12,i+9,QUIT_LINES[i][:-1])
+		method.bufferxy(columns/2 + 10,rows/2-16 + i+9,QUIT_LINES[i][:-1])
 
 #load GUI file.
 f=open("../resources/gui/gui.txt")
