efb1c383ef2134081ec6b50bc562229562f0a992
RedInquisitive <aj.walter@aol.com>
2015-12-09 20:46:51 -0600

    Just pass, no need to be fancy.

--- src/main.py
+++ src/main.py
@@ -175,7 +175,7 @@ def mainloop():
 		try:
 			time.sleep(sleepTime)
 		except IOError:
-			method.bufferxy(0,0,"&RKCatching up!&XX")
+			pass
 
 mainthread = threading.Thread(name='main', target=mainloop)
 mainthread.setDaemon(True)
