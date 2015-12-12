1e3f97b150f0d12f94c19bf91c7e65c1c0cfee21
RedInquisitive <aj.walter@aol.com>
2015-12-12 01:54:50 -0600

    saving

--- src/save.py
+++ src/save.py
@@ -6,7 +6,21 @@ def save(list):
 		elif type(i).__name__ == "str":
 			f.write("str:" + index + "\n")
 		elif type(i).__name__ == "list":
-			f.write("lst:" + "\n")
+			if(isinstance(b in a in index, list)):
+				f.write("2st:" + "\n")
+				for list1 in index:
+					for list2 in list1:
+						for element in list2:
+							f.write(element + "	")
+						f.write("\n")
+					f.write("\n")
+				f.write("end")
+			else:
+				f.write("1st:" + "\n")
+				for list1 in index:
+					for element in list1:
+						f.write(element + "	")
+					f.write("\n")
 		else:
 			f.write("not a real type\n")
 	close(f)
@@ -19,8 +33,10 @@ def load():
 			returnList.append(int(line[4:]))
 		elif line[0:4] == "str:":
 			returnList.append(str(line[4:]))
-		elif line[0:4] == "list:":
-			returnList.append("oh noe")
+		elif line[0:4] == "2st:":
+			returnList.append("oh noe 2 deee")
+		elif line[0:4] == "1st:":
+			returnList.append("oh noe 1 deee")
 		else:
 			returnList.append("not a real type")
 	return returnList
