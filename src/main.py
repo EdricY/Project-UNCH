6d4fb0651f2afabbeb81345e0022e827ef47c480
EdricY <yu.edric@gmail.com>
2015-12-12 02:37:26 -0600

    testing...

--- src/main.py
+++ src/main.py
@@ -263,7 +263,7 @@ while GAME_RUNNING:
 			quit()
 		elif ch=='n' or ch=='N':
 			quitMenuOpen = False
-	elif HERO_DISP_NUM != 0:
+	elif HERO_DISP_NUM != 0 and ch == "z":
 		HERO_DISP_NUM = 0
 		ch = " "
 		lastch = " "
