1bd0eff9be20bbf2efbeb0dce865d8b0f627e1b2
EdricY <yu.edric@gmail.com>
2015-12-13 00:39:49 -0600

    Update getch.py

--- src/getch.py
+++ src/getch.py
@@ -16,8 +16,7 @@ class _GetchUnix:
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(2)
-            ch = ch[1:]
+            ch = sys.stdin.read(1)
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
