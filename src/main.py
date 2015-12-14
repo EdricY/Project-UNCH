f3513f2bf56c8f007cb4c2f2e35136e9c11d1121
EdricY <yu.edric@gmail.com>
2015-12-14 16:31:22 -0600

    +

--- src/main.py
+++ src/main.py
@@ -215,7 +215,7 @@ def draw():
 			method.bufferxy(1,19,"Press &CXQ&XX to Quit.")
 			method.bufferxy(1,20,"Use &CX>&XX and &CX<&XX to attack (no need to press SHIFT)")
 		elif HERO_DISP_NUM != 0:
-			method.bufferxy(1, 18,"&YX"HEROES[HERO_DISP_NUM-1+4*HERO_SCREEN][0] + "&XX                                          ")
+			method.bufferxy(1, 18,"&YX"+HEROES[HERO_DISP_NUM-1+4*HERO_SCREEN][0] + "&XX                                          ")
 			for i in range(2):
 				method.bufferxy(1, 19+i,HERO_DESC[HERO_DISP_NUM-1+4*HERO_SCREEN][i])
 		else:
